bb1dc1403a3e50d7d19df66ba53f864aee1386a4
Ben Pilgrim <ben@pilgrim.me.uk>
2016-05-14 19:01:17 +0100

    Installed Laravel

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Site Maintenance</title>
+
+    <style>
+      body {
+        text-align: center;
+        padding: 20px;
+    }
+    @media (min-width: 768px){
+        body{
+            padding-top: 150px;
+        }
+    }
+    h1 {
+        font-size: 50px;
+    }
+    body {
+        font-size: 20px;
+        font-family: Helvetica, sans-serif;
+        color: #333;
+    }
+    article {
+        display: block;
+        text-align: left;
+        max-width: 650px;
+        margin: 0 auto;
+    }
+    a {
+        color: #dc8100;
+        text-decoration: none;
+    }
+    a:hover {
+        color: #333;
+        text-decoration: none;
+    }
+    footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        font-size: 12px;
+    }
+</style>
+</head>
+<body>
+    <article>
+        <h1>We&rsquo;ll be back soon!</h1>
+        <div>
+            <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:email@terragaming.co.uk">contact us</a>, otherwise we&rsquo;ll be back online soon&trade;!</p>
+            <p>- Terra Gaming Network</p>
+        </div>
+    </article>
+
+    <footer>
+      Server: core.terragaming.co.uk</footer>
+  </body>
+  </html>
